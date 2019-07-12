78a9f08af78c167477187b636e5a0400d49fe71b
Michael Schober <mike@getmigo.com>
2019-07-12 12:51:24 -0700

    moving methods

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,12 +1,12 @@
 import sys
-from text_processor import processAllCommands
+from text_processor import process_all_commands
 # python example_solution.py input_data.txt list_of_commands.txt output.txt
 
 if len(sys.argv) == 3:
   print 'Processing!'
   input_data = sys.argv[1];
   list_of_commands = sys.argv[2];
-  results = processAllCommands(input_data, list_of_commands)
+  results = process_all_commands(input_data, list_of_commands)
   print results
 else:
   print 'Expecting 2 arguments'
--- text_processor.py
+++ text_processor.py
@@ -1,10 +1,13 @@
-def processAllCommands(input_data, commands):
-  commands_list = []
-  command = '<placeholder>'
 
-  with(open(commands, 'r')) as f:
+def read_commands(list_of_commands):
+  commands_list = []
+  with(open(list_of_commands, 'r')) as f:
     command = f.readline()
     while command:
       commands_list.append(command)
       command = f.readline()
+  return commands_list
+
+def process_all_commands(input_data, list_of_commands):
+  commands_list = read_commands(list_of_commands)
   print 'going to scrape!', commands_list
