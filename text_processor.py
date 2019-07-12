1b82ea8b35d1f8e62405cdce750a548dd6f7a3b3
Michael Schober <mike@getmigo.com>
2019-07-12 16:40:22 -0700

    starting to parse through the freq commands

--- text_processor.py
+++ text_processor.py
@@ -17,25 +17,46 @@ def process_frequency_command(input_data, args):
     return freq_req_map
   freq_req_map = fill_freq_request_map(args)
 
-  freq_map = {}
+  def read_text(input_data):
+    '''
+    1. Reads the lines of text from the file and splits into tokens.
+    '''
+    text = None
+    with(open(input_data)) as f:
+      text = f.read().split()
+    return text
+
+  def count_word_freq(text, freq_req_map):
+    '''
+    1. 
+    '''
+    freq_map = {}
+    pos = 0
+    for word in text:
+      if word in freq_req_map:
+        if word in freq_map:
+          freq_map[word]['cnt'] += 1
+        else:
+          freq = {
+            'pos': pos,
+            'cnt': 1
+          }
+          freq_map[word] = freq
+          pos += 1
+    return freq_map
+
   print 'Start frequency command'
-  text = None
-  with(open(input_data)) as f:
-    text = f.read().split()
+  text = read_text(input_data)
+  freq_map = count_word_freq(text, freq_req_map)
 
-  pos = 0
-  for word in text:
-    if word in freq_req_map:
-      if word in freq_map:
-        freq_map[word]['cnt'] += 1
-      else:
-        freq = {
-          'pos': pos,
-          'cnt': 1
-        }
-        freq_map[word] = freq
-        pos += 1
-
+  #TODO: need to figure out how to increment pos if a request word isn't found anywhere in the text
+  for word in freq_req_map:
+    if not word in freq_map:
+      print word
+      # freq_map[word] = {
+      #   ''
+      # }
+      
   print text
   print freq_req_map
   print freq_map
