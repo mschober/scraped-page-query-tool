6ae484dd2c822e39efacff818969b6a066aa5ecc
Michael Schober <mike@getmigo.com>
2019-07-12 12:45:59 -0700

    gets the list of commands from the file

--- text_processor.py
+++ text_processor.py
@@ -1,2 +1,10 @@
 def processAllCommands(input_data, commands):
-  print 'going to scrape!'
+  commands_list = []
+  command = '<placeholder>'
+
+  with(open(commands, 'r')) as f:
+    command = f.readline()
+    while command:
+      commands_list.append(command)
+      command = f.readline()
+  print 'going to scrape!', commands_list
