cb8d76a96d808517abfefaffba218834078e71cc
Michael Schober <mike@getmigo.com>
2019-07-12 13:03:31 -0700

    able to call the right type of processor for each command

--- text_processor.py
+++ text_processor.py
@@ -1,13 +1,25 @@
+def process_frequency_command(input_data, args):
+  print 'Start frequency command'
+def process_top_command(input_data, args):
+  print 'Start top command'
+def process_inorder_command(input_data, args):
+  print 'Start inorder command'
 
-def read_commands(list_of_commands):
-  commands_list = []
-  with(open(list_of_commands, 'r')) as f:
-    command = f.readline()
-    while command:
-      commands_list.append(command)
-      command = f.readline()
-  return commands_list
 
 def process_all_commands(input_data, list_of_commands):
-  commands_list = read_commands(list_of_commands)
-  print 'going to scrape!', commands_list
+  results = []
+  for command in open(list_of_commands):
+    split_command = command.split(' ')
+    command_type = split_command[0]
+    args = split_command[1:]
+    print command_type, args
+    if command_type == 'FREQUENCY':
+      result = process_frequency_command(input_data, args)
+    elif command_type == 'TOP':
+      result = process_top_command(input_data, args)
+    elif command_type == 'IN_ORDER':
+      result = process_inorder_command(input_data, args)
+    else:
+      raise Exception('Error: unknown command type {}'.format(command_type))
+    results.append(result)
+  print 'Done!'
